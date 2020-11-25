56e211804a0ae9f5859f7a6839900faa11cd1a67
张佳林 <getling@yeah.net>
2020-11-25 15:26:11 +0800

    [MOD]修改泛型

--- gwframe/src/main/java/com/getling/gwframe/http/parser/DataListResponse.kt
+++ gwframe/src/main/java/com/getling/gwframe/http/parser/DataListResponse.kt
@@ -11,5 +11,5 @@ class DataListResponse<T> : Serializable {
     var flag = 0
     var errorMessage: String? = null
     var logMessage: String? = null
-    var dataList: T? = null
+    var dataList: List<T>? = null
 }
